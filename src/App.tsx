ba8c00a15bb0451c9535c2cc8dd7307e93272470
Kamil Grabek <kamil.grabek@allegro.pl>
2020-06-17 13:12:27 +0200

    simplified App

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,7 @@
 import React from 'react';
 
-function GrowingButton({ label, visible }) {
-  if (!visible) {
-    return null;
-  }
-
-  function grow() {
-    console.log(['grow']);
-  }
-
-  return (
-    <button style={{ fontSize: '15px' }} onClick={grow}>
-      {label}
-    </button>
-  );
-}
-
-function App() {
+function GrowingButton() {
   const count = 0;
-  const visible = true;
-
-  function disappear() {
-    console.log(['disappear']);
-  }
-
-  function appear() {
-    console.log(['appear']);
-  }
 
   function increase() {
     console.log(['increase']);
@@ -38,18 +13,20 @@ function App() {
 
   return (
     <div>
-      <h1>My Own React App!</h1>
       <div>
-        <div>
-          <span>{count.toString()}</span>
-          <button onClick={increase}>increase counter</button>
-          <button onClick={decrease}>decrease counter</button>
-        </div>
-        <GrowingButton label="Click it like it's hot!" visible={visible} />
-        <button onClick={visible ? disappear : appear}>{`${
-          visible ? 'disappear' : 'appear'
-        } now!`}</button>
+        <span>{count.toString()}</span>
+        <button onClick={increase}>increase counter</button>
+        <button onClick={decrease}>decrease counter</button>
       </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>My Own React App!</h1>
+      <GrowingButton />
     </div>
   );
 }
