c75cc47fb6c42432a719022d6c97cb47db0a7063
Kamil Grabek <kamil.grabek@allegro.pl>
2020-09-15 21:13:08 +0200

    Updates

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
-import App from './App.tsx';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-window.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<App />, document.getElementById('root'));
-});
