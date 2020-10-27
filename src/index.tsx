820b441456f32de4cac22ced5d2ff3a08e5317ce
Kamil Grabek <kamil.grabek@allegro.pl>
2020-10-27 17:16:46 +0100

    dodanie możliwosci wyboru scenariusza przez query + simplest scenario

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,19 @@ import App from './App.tsx';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const params = new URLSearchParams(window.location.search);
+
+function getElementToRender(scenarioName: string) {
+  if (scenarioName === 'simplest') {
+    return <span>Simplest React App</span>;
+  }
+
+  return <App />;
+}
+
 window.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<App />, document.getElementById('root'));
+  ReactDOM.render(
+    getElementToRender(params.get('scenario')),
+    document.getElementById('root'),
+  );
 });
